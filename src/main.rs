42bb028bdff1d0224c2e44263100ef3011426932
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-12-23 18:32:19 +0000

    Working modbus-proxy
    - single client
    - no buffer handling
    - very poor error handling

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,47 @@
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
     loop {
-        let (mut socket, _) = listener.accept().await?;
+        let (mut client, _) = listener.accept().await?;
+        let mut modbus = TcpStream::connect("127.0.0.1:5030").await?;
 
         tokio::spawn(async move {
             let mut buf = [0; 1024];
 
-            // In a loop, read data from the socket and write the data back.
+            // In a loop, read data from the client and write the data back.
             loop {
-                let n = match socket.read(&mut buf).await {
+                let n = match client.read(&mut buf).await {
                     // socket closed
                     Ok(n) if n == 0 => return,
                     Ok(n) => n,
                     Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
+                        eprintln!("failed to read from client; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                if let Err(e) = modbus.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to modbus; err = {:?}", e);
+                    return;
+                }
+
+                let n = match modbus.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from modbus; err = {:?}", e);
                         return;
                     }
                 };
 
                 // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
+                if let Err(e) = client.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to client; err = {:?}", e);
                     return;
                 }
             }
