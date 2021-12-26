1d6ddc380d4eff20b708505a2536feada0da3c61
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-12-26 11:35:04 +0000

    Define Result types

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,30 @@
-use std::error::Error;
-use tokio::io::{AsyncReadExt, AsyncWriteExt, Result};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, Result as IOResult};
 use tokio::net::{TcpListener, TcpStream};
 
-trait Packet {
-    fn payload_size(&self) -> std::result::Result<usize, Box<dyn Error>>;
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
 
-    fn total_size(&self) -> std::result::Result<usize, Box<dyn Error>> {
+trait Packet {
+    fn payload_size(&self) -> Result<usize>;
+
+    fn total_size(&self) -> Result<usize> {
         Ok(6 + self.payload_size()?)
     }
 
-    fn packet(&self) -> std::result::Result<&[u8], Box<dyn Error>>;
+    fn packet(&self) -> Result<&[u8]>;
 }
 
 impl Packet for [u8] {
-    fn payload_size(&self) -> std::result::Result<usize, Box<dyn Error>> {
+    fn payload_size(&self) -> Result<usize> {
         Ok(u16::from_be_bytes(self[4..6].try_into()?) as usize)
     }
-    fn packet(&self) -> std::result::Result<&[u8], Box<dyn Error>> {
+    fn packet(&self) -> Result<&[u8]> {
         let size = self.total_size()?;
         Ok(&self[0..size])
     }
 }
 
-async fn read_packet_into(
-    stream: &mut TcpStream,
-    buf: &mut [u8],
-) -> std::result::Result<usize, Box<dyn Error>> {
+async fn read_packet_into(stream: &mut TcpStream, buf: &mut [u8]) -> Result<usize> {
     // Read header
     stream.read_exact(&mut buf[0..6]).await?;
     // calculate payload size
@@ -35,14 +34,11 @@ async fn read_packet_into(
     Ok(total_size)
 }
 
-async fn write_packet(stream: &mut TcpStream, buf: &[u8], size: usize) -> Result<()> {
+async fn write_packet(stream: &mut TcpStream, buf: &[u8], size: usize) -> IOResult<()> {
     stream.write_all(&buf[0..size]).await
 }
 
-async fn handle_client(
-    mut client: &mut TcpStream,
-    mut modbus: &mut TcpStream,
-) -> std::result::Result<(), Box<dyn Error>> {
+async fn handle_client(mut client: &mut TcpStream, mut modbus: &mut TcpStream) -> Result<()> {
     let mut buf = [0; 8192];
     loop {
         let size = read_packet_into(&mut client, &mut buf).await?;
@@ -62,7 +58,7 @@ struct Modbus {
     modbus_address: String,
 }
 
-async fn server(modbus: Modbus) -> Result<()> {
+async fn server(modbus: Modbus) -> IOResult<()> {
     let listener = TcpListener::bind(modbus.bind_address).await?;
 
     loop {
@@ -82,7 +78,7 @@ async fn server(modbus: Modbus) -> Result<()> {
 }
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<()> {
+async fn main() -> IOResult<()> {
     let modbus = Modbus {
         bind_address: "127.0.0.1:8080".to_string(),
         modbus_address: "127.0.0.1:5030".to_string(),
