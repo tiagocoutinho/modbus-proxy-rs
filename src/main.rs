73e354ef5c9bbfe102f22cf578882479d9e0e0c7
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-12-26 18:50:44 +0000

    Handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ use tokio::net::{
     tcp::{OwnedReadHalf, OwnedWriteHalf},
     TcpListener, TcpStream,
 };
+use tokio::sync::{mpsc, oneshot};
 
 type Error = Box<dyn std::error::Error>;
 type Result<T> = std::result::Result<T, Error>;
+type Frame = Vec<u8>;
+type Sender = mpsc::Sender<(Frame, oneshot::Sender<Frame>)>;
+type Receiver = mpsc::Receiver<(Frame, oneshot::Sender<Frame>)>;
 
 struct Connection {
     writer: BufWriter<OwnedWriteHalf>,
@@ -25,50 +29,31 @@ impl Connection {
         // Read header
         self.reader.read_exact(&mut buf[0..6]).await?;
         // calculate payload size
-        let total_size = buf.total_size()?;
+        let total_size = 6 + frame_size(&buf)?;
         self.reader.read_exact(&mut buf[6..total_size]).await?;
         Ok(total_size)
     }
 
-    async fn write_frame(&mut self, buf: &[u8]) -> IOResult<()> {
-        self.writer.write_all(buf).await?;
+    async fn write_frame(&mut self, buf: Frame) -> IOResult<()> {
+        self.writer.write_all(&buf[..]).await?;
         self.writer.flush().await?;
         Ok(())
     }
 }
 
-trait Packet {
-    fn payload_size(&self) -> Result<usize>;
-
-    fn total_size(&self) -> Result<usize> {
-        Ok(6 + self.payload_size()?)
-    }
-
-    fn packet(&self) -> Result<&[u8]>;
+fn frame_size(frame: &[u8]) -> Result<usize> {
+    Ok(u16::from_be_bytes(frame[4..6].try_into()?) as usize)
 }
 
-impl Packet for [u8] {
-    fn payload_size(&self) -> Result<usize> {
-        Ok(u16::from_be_bytes(self[4..6].try_into()?) as usize)
-    }
-    fn packet(&self) -> Result<&[u8]> {
-        let size = self.total_size()?;
-        Ok(&self[0..size])
-    }
-}
-
-async fn handle_client(client: &mut Connection, modbus: &mut Connection) -> Result<()> {
-    let mut buf = [0; 8192];
+async fn handle_client(client: &mut Connection, channel: Sender) -> Result<()> {
+    let mut buf = [0; 8 * 1024];
     loop {
         let size = client.read_frame_into(&mut buf).await?;
+        let (tx, rx) = oneshot::channel();
+        channel.send((buf[0..size].to_vec(), tx)).await.unwrap();
+        let frame = rx.await.unwrap();
 
-        // Write all
-        modbus.write_frame(&buf[0..size]).await?;
-
-        let size = modbus.read_frame_into(&mut buf).await?;
-
-        // Write all
-        client.write_frame(&buf[0..size]).await?;
+        client.write_frame(frame).await?;
     }
 }
 
@@ -77,19 +62,33 @@ struct Config {
     modbus_address: String,
 }
 
+async fn run(modbus_address: String, channel: &mut Receiver) {
+    let modbus = TcpStream::connect(modbus_address).await.unwrap();
+    let mut modbus = Connection::new(modbus);
+    let mut buf = [0; 8 * 1024];
+    while let Some((frame, client)) = channel.recv().await {
+        modbus.write_frame(frame).await;
+        let size = modbus.read_frame_into(&mut buf).await.unwrap();
+        client.send(buf[0..size].to_vec());
+    }
+}
+
 async fn server(config: Config) -> IOResult<()> {
     let listener = TcpListener::bind(config.bind_address).await?;
+    let (tx, mut rx) = mpsc::channel(32);
+
+    let modbus_address = config.modbus_address;
+    tokio::spawn(async move {
+        run(modbus_address, &mut rx).await;
+    });
 
     loop {
         let (client, client_addr) = listener.accept().await?;
-        let addr = config.modbus_address.clone();
-        let modbus_stream = TcpStream::connect(addr).await?;
-        modbus_stream.set_nodelay(true)?;
         client.set_nodelay(true)?;
-        let mut modbus = Connection::new(modbus_stream);
         let mut client = Connection::new(client);
+        let tx = tx.clone();
         tokio::spawn(async move {
-            match handle_client(&mut client, &mut modbus).await {
+            match handle_client(&mut client, tx).await {
                 Err(err) => eprintln!("Error {}: {:?}", client_addr, err),
                 _ => {}
             }
